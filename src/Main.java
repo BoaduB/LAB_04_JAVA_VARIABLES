726ec534f6fd40e3ebc699340b714c3307ea2ada
BoaduB <boansibk@mail.uc.edu>
2023-02-02 00:13:20 -0500

    LAB_04_JAVA_VARIABLES

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        double myLunchCost = 12.50;
+        int kidsNumber = 0;
+        boolean isRaining = false;
+        double gallonGasPrice = 3.99;
+        int favNumber = 9;
+        double shoeSize = 9.5;
+        String birthMonth = "March";
+        String fullName = "Boadu Boansi";
+
+        System.out.println("The cost if my luncg is: " + myLunchCost);
+        System.out.println("The number if kids in my family is: " + kidsNumber);
+        System.out.println("It is raining outside: " + isRaining);
+        System.out.println("The price of a gallon of gas: " + gallonGasPrice);
+        System.out.println("My favorite number is: " + favNumber);
+        System.out.println("My shoe size is: " + shoeSize);
+        System.out.println("My birth month is: " + birthMonth);
+        System.out.println("My full name is: " + fullName);
+
+
+
+
+
+    }
+}
+
